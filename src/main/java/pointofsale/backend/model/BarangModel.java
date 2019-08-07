3c7ab303e0cb55dfffb05c7cda6b4e6b9bddc72e
rasyidmm <37688419+rasyidmm@users.noreply.github.com>
2019-08-07 16:49:24 +0700

    createModel

---
+++ src/main/java/pointofsale/backend/model/BarangModel.java
@@ -0,0 +1,14 @@
+package pointofsale.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+@Entity
+public class BarangModel implements Serializable {
+    private static long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+}
---
+++ src/main/java/pointofsale/backend/model/MemberModel.java
@@ -0,0 +1,12 @@
+package pointofsale.backend.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class MemberModel implements Serializable {
+    private static long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+}
