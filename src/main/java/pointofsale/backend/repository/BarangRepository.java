d0ca664a02330c83272f908db8964033beffa77d
rasyidmm <37688419+rasyidmm@users.noreply.github.com>
2019-08-25 20:38:48 +0700

    Update BarangRepository.java

--- src/main/java/pointofsale/backend/repository/BarangRepository.java
+++ src/main/java/pointofsale/backend/repository/BarangRepository.java
@@ -11,8 +11,8 @@ import java.util.List;
 @Repository
 public interface BarangRepository extends JpaRepository<BarangModel, Long> {
     List<BarangModel>getAllByStatus(String Status);
-    @Query(value = "select * from barang_model a JOIN kategory_barang_model c on a.kategory_barang_model_id = c.id WHERE c.id=:id ", nativeQuery = true)
-    List<BarangModel>getAllByProdusenBarangModelId(Long id);
     @Query(value = "select * from barang_model a JOIN produsen_barang_model c on a.produsen_barang_model_id = c.id WHERE c.id=:id ", nativeQuery = true)
+    List<BarangModel>getAllByProdusenBarangModelId(Long id);
+    @Query(value = " select * from barang_model a JOIN kategory_barang_model c on a.kategory_barang_model_id = c.id WHERE c.id=:id", nativeQuery = true)
     List<BarangModel>getAllByKategoryBarangModelOrderById(Long id);
 }
